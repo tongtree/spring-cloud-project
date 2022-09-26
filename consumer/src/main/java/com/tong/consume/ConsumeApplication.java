a0c65529a61ec3557c1a43911efed9db8c7529d5
niutong <tong17733603273>
2022-09-26 16:56:32 +0800

    init

--- consumer/src/main/java/com/tong/consume/ConsumeApplication.java
+++ consumer/src/main/java/com/tong/consume/ConsumeApplication.java
@@ -1,5 +1,6 @@
 package com.tong.consume;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -8,6 +9,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@MapperScan("com.tong.consume.mapper")
 public class ConsumeApplication {
 
     public static void main(String[] args) {
--- consumer/src/main/java/com/tong/consume/controller/UserController.java
+++ consumer/src/main/java/com/tong/consume/controller/UserController.java
@@ -2,6 +2,9 @@ package com.tong.consume.controller;
 
 import com.tong.consume.bean.base.ResponseUtil;
 import com.tong.consume.entity.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,12 +19,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/user")
 public class UserController {
+    private static final Logger log = LoggerFactory.getLogger(UserController.class);
+
     /**
      * 登录
      *
      * @return
      */
+    @GetMapping("/login")
     public ResponseUtil<User> login() {
+        log.info("info---");
+        log.error("error---");
+        log.debug("debug---");
+        try{
+            throw new RuntimeException("runtimeException---");
+        }catch(Exception e){
+            log.error("异常",e);
+        }
         User user = new User();
         return ResponseUtil.success(user);
     }
