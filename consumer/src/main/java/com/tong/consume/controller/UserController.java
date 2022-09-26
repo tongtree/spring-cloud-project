955e884f418b32e748b20599407ac9e5e8ca7ee7
niutong <tong17733603273>
2022-09-26 17:18:22 +0800

    init

--- consumer/src/main/java/com/tong/consume/controller/UserController.java
+++ consumer/src/main/java/com/tong/consume/controller/UserController.java
@@ -2,11 +2,15 @@ package com.tong.consume.controller;
 
 import com.tong.consume.bean.base.ResponseUtil;
 import com.tong.consume.entity.User;
+import com.tong.consume.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * <p>
@@ -21,6 +25,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     private static final Logger log = LoggerFactory.getLogger(UserController.class);
 
+    @Autowired
+    private UserService userService;
+
     /**
      * 登录
      *
@@ -28,13 +35,14 @@ public class UserController {
      */
     @GetMapping("/login")
     public ResponseUtil<User> login() {
+        List<User> list = userService.lambdaQuery().eq(User::getUserName, "").list();
         log.info("info---");
         log.error("error---");
         log.debug("debug---");
-        try{
+        try {
             throw new RuntimeException("runtimeException---");
-        }catch(Exception e){
-            log.error("异常",e);
+        } catch (Exception e) {
+            log.error("异常", e);
         }
         User user = new User();
         return ResponseUtil.success(user);
