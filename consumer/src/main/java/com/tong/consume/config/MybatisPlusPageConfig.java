7635a9ceeabc5df10d3dca04acfb00cf8c0ba91a
niutong <tong17733603273>
2022-10-11 16:47:39 +0800

    mybatispuls 分页查询

---
+++ consumer/src/main/java/com/tong/consume/config/MybatisPlusPageConfig.java
@@ -0,0 +1,30 @@
+package com.tong.consume.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * MyBatisPlus 分页插件配置
+ *
+ * @Author gblfy
+ * @Date 2022-05-04 14:41
+ **/
+@Configuration
+public class MybatisPlusPageConfig {
+
+    /**
+     * 分页插件（官网最新）
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+
+}
+
+
